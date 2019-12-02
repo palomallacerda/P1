33d382153017a52bc96b41bca7f1dc3f3449bf70
palomallacerda <56915694+palomallacerda@users.noreply.github.com>
2019-12-02 18:13:25 -0300

    Update array.c

--- array.c
+++ array.c
@@ -24,38 +24,73 @@ void imprimir (int i, int vetor[])
 
 }
 
-void deslocamento (int i, int array, int n)
+void deslocamento (int i, int array[], int n)
+{
+	if (i < n - 1)
+	{
+		int aux = array[i];
+		array[i] = array[i+1];
+		array[i+1] = aux;
+		deslocamento(i+1, array, n);
+	}
+
+}
+
+void printar_deslocamento (int i, int array[], int n)
 {
 	if (i < n)
 	{
-		int aux = array[i];
-		array[i] = array[n-1];
-		array[n-1] = array[i];
+		printf("%d ", array[i]);
+		printar_deslocamento(i + 1, array, n);
 	}
-	deslocamento(i+1, array, n);
+	else
+	{
+		printf("\n");
+		return;
+	}
 }
 
-/*void printar_deslocamento (int i, int array, int n)
+void boubble(int i, int n, int array[])
 {
-	if (i > 0)
-	{
-		printf("%d\n", array[i]);
-		printar_deslocamento(i)
-	}
+	
+    if (i < n)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int aux = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = aux;
+        }
+        boubble(i + 1, n, array);
+    }
+}
 
-}*/
+void imprimir_decres(int i, int n, int array[])
+{
+	if (n < i)
+	{
+		return;
+	}
+	else
+	{
+		printf("%d ", array[n]);
+		imprimir_decres(i, n-1, array);
+	}
+}
+
 int main()
 {
 	int n;
 	scanf("%d", &n);
-
 	int vetor[n];
-
-	ler(0, n, vetorz);
+	
+	ler(0, n, vetor);
 	imprimir(n-1, vetor);
 
+	deslocamento(0, vetor, n);
+	printar_deslocamento(0, vetor, n);
 
-	deslocamento(0, array, n);
-
+	boubble(0, n, vetor);
+	imprimir_decres(0, n-1, vetor);
 	return 0;
 }
